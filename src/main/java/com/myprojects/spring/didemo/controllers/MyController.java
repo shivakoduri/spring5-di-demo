d6d05dec00d2d6fde62cedf4501140a7e9340999
shiva <koduri.shiva@gmail.com>
2018-01-25 22:33:01 +0530

    Adding Controller

---
+++ src/main/java/com/myprojects/spring/didemo/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.myprojects.spring.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!");
+
+        return "foo";
+    }
+}
