c04b167df55a2a0526679e440a09a70d7928bfd0
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-03-26 21:17:32 +0530

    Demo for enviroment property

--- src/main/java/com/myprojects/spring/didemo/config/PropertyConfig.java
+++ src/main/java/com/myprojects/spring/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.myprojects.spring.didemo.config;
 
 import com.myprojects.spring.didemo.examplebeans.SomeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${app.database.userName}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public SomeDataSource getDataSource(){
         SomeDataSource dataSource = new SomeDataSource();
-        dataSource.setUser(user);
+        dataSource.setUser(env.getProperty("USERNAME"));
         dataSource.setPassword(password);
         dataSource.setUrl(url);
 
